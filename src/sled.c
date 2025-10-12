0576e93796fa1419f970ed7bafe19eb470f9a104
Georges Kadlubski <georgeskadlubski@gmail.com>
2025-10-12 13:37:46 +0200

    Add: programm structure

---
+++ src/sled.c
@@ -0,0 +1,20 @@
+//###########################################################################
+//# Single line editor (SLED) is a simple editor to edit a single line of   #
+//# a file directly in the command line prompt.                             #
+//# https://github.com/kadlubski88/single_line_editor                       #
+//#                                                                         #
+//# The MIT License (MIT)                                                   #
+//# Copyright © 2025 Georges Kadlubski                                      #
+//# URL: https://mit-license.org/                                           #
+//#                                                                  v0.0.1 #
+//###########################################################################
+
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <poll.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
